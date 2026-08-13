7d96143842bddc5d0f7ce1669bf6f25c71c24167
jjdevasurya2009-blip <jjdevasurya2009@gmail.com>
2026-08-13 16:17:21 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -76,10 +76,10 @@ async def handle_Query(bot: Client, query: CallbackQuery):
         Btn = [
             [InlineKeyboardButton(text='❗Hᴇʟᴘ', callback_data='help'), InlineKeyboardButton(
                 text='🌀Sᴇʀᴠᴇʀ Sᴛᴀᴛs', callback_data='server')],
-            [InlineKeyboardButton(text='🌻Uᴘᴅᴀᴛᴇs', url='https://t.me/urstarkz'),
+            [InlineKeyboardButton(text='🌻Uᴘᴅᴀᴛᴇs', url='https://t.me/KNIGHTSS_CLAN'),
              InlineKeyboardButton(text='🌨️Aʙᴏᴜᴛ', callback_data='about')],
             [InlineKeyboardButton(text='❄️Dᴇᴠᴇʟᴏᴘᴇʀ',
-                                  url='https://t.me/urstarkz')]
+                                  url='https://t.me/Last_uchihaz')]
         ]
 
         await query.message.edit(text=Txt.START_MSG.format(query.from_user.mention), reply_markup=InlineKeyboardMarkup(Btn))
